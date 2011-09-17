b06934dec2fd3b82e25d1adbdff185f115e6a731
Emeka Nwankwo <emekamicro@gmail.com>
2011-09-17 18:14:40 +0200

    Edited dictj.py via GitHub

--- dictj.py
+++ dictj.py
@@ -1,12 +1,8 @@
 import os
 from string import find
+
 currentdir = os.path.curdir
-
 alphabet_dict = {'a':1, 'b':1, 'c':1, 'd':1, 'e':1, 'f':3, 'g':3, 'h':2, 'i':1, 'j':4, 'k':2, 'l':1, 'm':4, 'n':5, 'o':1, 'p':2, 'q':9, 'r':4, 's':2, 't':3, 'u':8, 'v':9, 'w':8, 'x':7, 'y':5, 'z':9 }
-#print result_dict['g']  
-#masterword = function-call()
-
-
 masterlist = []
 word_list = []
 searchwordlist = []
@@ -31,32 +27,23 @@ def start_up():
      mycount = 0
      for wordn in wordnlist:
        wordn = wordn.strip()
-      # if wordn.find("\'"):
-       #   continue 
        if len(wordn) > 2 and len(wordn) < 14 :
           temp_list.append(wordn )
           if mycount == 20:
             sub_list =  temp_list[:]
             word_list.append(sub_list)
-            temp_list = []
-            mycount = 0
-            sub_list = [] 
+            sub_list, mycount, temp_list  = ([], 0, [])
        mycount = mycount + 1            
        if len(wordn) > 4 and len(wordn) < 14:
           masterlist.append(wordn)
-     #wordnlist.close()
      if temp_list:
          word_list.append(temp_list)
-     #random.shuffle(masterlist)
 
 start_up()
 
-#list_path = os.path.join(currentdir, "bb\\mprocessed\\new\\")
 def processed (my_tree, my_word):
-        local_list = []
-        if cmp_count(my_word, my_tree):
-             local_list.append(my_word)
-        return local_list
+        return ([], [].append(my_word))[cmp_count(my_word, my_tree)]
+
              
 def update_searchlist(wordi):
      #search_path = os.path.join(list_path, wordi)
@@ -64,10 +51,6 @@ def update_searchlist(wordi):
       tree = make_tree(wordi)
       given_letters = frozenset(wordi)
       searchwordlist = sum([ processed(tree, wordn)  for sub_list in word_list for wordn  in sub_list if given_letters.issuperset(frozenset(wordn)) ], [])
-
-     
-#update_searchlist("abased")
-#print searchwordlist    
 
 
 searchedwordset = set()
@@ -80,10 +63,7 @@ def compute_result(downmessage):
          global score_value
         # global 
          message_str = "".join(downmessage)
-         if "1" in message_str:
-             one_index = message_str.index("1")
-         else:
-             one_index = len(message_str)
+         one_index = message_str.index("1") if "1" in message_str else len(message_str)
          target_str = message_str[:one_index]
          #print target_str
          if target_str in searchwordlist:
@@ -97,8 +77,6 @@ def compute_result(downmessage):
                     return -2
                     
 def word_not_found(word):
-      if word in searchedwordset:
-        return 0
-      else:
-        return 1
+      return  word not in searchedwordset:
+
 
